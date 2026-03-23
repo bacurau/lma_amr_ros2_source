6dd22327d3bfdd1d2a6ce075b907f450f18e9d3c
robo-lma <robo.lma.unicamp@gmail.com>
2026-03-23 14:12:56 +0000

    Allow user to run the project instead of building and running everytime.

--- scripts/main.sh
+++ scripts/main.sh
@@ -10,11 +10,18 @@
 
 ## This script closes the vscode terminal if it is executed with source ./file.sh or . ./file.sh. 
 # It might no close on the first execution, but it can close on later executions.
-
-
 set -euo pipefail
+YES=0
+NO=1
+RUN_PROJECT="${2:-$YES}"
+build_type="${1-Release}"
 scripts_path="./" # Path to the scripts folder
-$scripts_path/install_dependencies.sh # Install dependencies
-cd ..; colcon build --symlink-install; cd ./scripts  # Build ROS2 packages
+#$scripts_path/install_dependencies.sh # Install dependencies
+cd ..; colcon build --symlink-install  --cmake-args "-DCMAKE_BUILD_TYPE=$build_type" ; cd ./scripts  # Build ROS2 packages
 $scripts_path/create_and_build_microROS_agent.sh # Create and build micro-ROS agent
-$scripts_path/run_project.sh # Run all ros2 packages from the AMR project (lma_amr_ros2_source)
+
+if [[ "$RUN_PROJECT" =  "$YES" ]]; then
+    $scripts_path/run_project.sh # Run all ros2 packages from the AMR project (lma_amr_ros2_source)
+else 
+    export ROS_DOMAIN_ID=25
+fi
