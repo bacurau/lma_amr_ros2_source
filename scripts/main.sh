5a5aef24a438b1d0b033afb3cdd72128c7a1889a
robo-lma <robo.lma.unicamp@gmail.com>
2026-03-26 12:55:44 +0000

    Modified build script to allow the user to choose the build type and to run the project without the need to rebuild every time.

--- scripts/main.sh
+++ scripts/main.sh
@@ -11,10 +11,11 @@
 ## This script closes the vscode terminal if it is executed with source ./file.sh or . ./file.sh. 
 # It might no close on the first execution, but it can close on later executions.
 set -euo pipefail
+export ROS_DOMAIN_ID=25
 YES=0
 NO=1
 RUN_PROJECT="${2:-$YES}"
-build_type="${1-Release}"
+build_type="${1:-Release}"
 scripts_path="./" # Path to the scripts folder
 $scripts_path/install_dependencies.sh # Install dependencies
 cd ..; colcon build --symlink-install  --cmake-args "-DCMAKE_BUILD_TYPE=$build_type" ; cd ./scripts  # Build ROS2 packages
@@ -22,6 +23,4 @@ $scripts_path/create_and_build_microROS_agent.sh # Create and build micro-ROS ag
 
 if [[ "$RUN_PROJECT" =  "$YES" ]]; then
     $scripts_path/run_project.sh # Run all ros2 packages from the AMR project (lma_amr_ros2_source)
-else 
-    export ROS_DOMAIN_ID=25
 fi
